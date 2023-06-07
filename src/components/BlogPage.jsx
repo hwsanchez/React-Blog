23dbe07321eef78bcfc5ae92b4d771351d6f65e2
Hugo Sanchez <hwsanchez.poreda@gmail.com>
2023-06-07 13:13:13 +0200

    Added Mapbox Wrapper at /blog

--- src/components/BlogPage.jsx
+++ src/components/BlogPage.jsx
@@ -1,7 +1,24 @@
+import Map from "react-map-gl";
+
 const BlogPage = () => {
   return (
-    <h1>BLOG Page</h1>
-  )
-}
+    <>
+      <h1>BLOG Page</h1>
+      <br />
+      <br />
+      {/* Mapbox Test! */}
+      <Map
+        initialViewState={{
+          longitude: -122.4,
+          latitude: 37.8,
+          zoom: 14,
+        }}
+        style={{ width: 600, height: 400 }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxAccessToken={import.meta.env.VITE_MAPBOX_TOKEN}
+      />
+    </>
+  );
+};
 
 export default BlogPage;
